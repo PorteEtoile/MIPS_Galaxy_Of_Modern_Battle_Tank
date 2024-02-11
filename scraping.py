0fb7e3c60d6fed9e3316cb695a7ca63eaedb0bfd
PorteEtoile <95546962+PorteEtoile@users.noreply.github.com>
2024-02-11 11:26:05 +0100

    Create scraping.py

---
+++ scraping.py
@@ -0,0 +1,28 @@
+# https://en.wikipedia.org/wiki/List_of_Russo-Ukrainian_War_military_equipment
+# https://en.wikipedia.org/
+# h4 = Tanks
+# h2 =  Forces
+import os
+import requests
+import scrapy
+import re
+from bs4 import BeautifulSoup
+
+def getListTank():
+    linkList = "https://en.wikipedia.org/wiki/List_of_Russo-Ukrainian_War_military_equipment"
+    response = requests.get(linkList)
+    if response.status_code == 200:
+        print("Access to the Tank's list")
+        soupList = BeautifulSoup(response.text, 'html.parser')
+        pageStuff = soupList.find("div",class_="mw-content-ltr mw-parser-output")
+        h4_tags = pageStuff.find_all('h4')
+        for h4_tag in h4_tags:
+            if h4_tag.text == "Tanks[edit]":
+                next_ul = h4_tag.find_next_sibling('ul')
+                if next_ul :
+                    li_tags = next_ul.find_all('li')
+                    for li_tag in li_tags:
+                        linkTank = li_tag.find('a')
+                        print(linkTank)
+
+getListTank()
